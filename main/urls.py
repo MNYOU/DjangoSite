eb3cc3c28a41816c43b0983472c52e76b3a5f57b
MNYOU <mukanovarman777@gmail.com>
2023-01-16 01:51:26 +0600

    стилизация

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', home),
-    path('main/', index),
     path('base', base),
     path('list/<int:vacancy_id>', vacancies),
     path('last', last_vacancies),
-    path('relevance/', relevance)
+    path('relevance/', relevance),
+    path('geography', geography)
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import random
 import re
 import openpyxl
 import os
@@ -18,49 +19,32 @@ def base(request):
 
 
 def home(request):
-    vacancies = Vacancy.objects.all()
-    return render(request, 'main/index.html', {"name": "Anna", "title": "Тестовая страница"})
-
-
-def index(request):  # HttpRequest
-    vacancies = Vacancy.objects.all()  # from database
-    print("hello world")
-    res = f'<div><h3>{vacancies[0].name}</h3></div>'
-    return HttpResponse(res)
-    # return HttpResponse("страница приложения main")
+    return render(request, 'main/index.html', {"title": "Главная страница"})
 
 
 def vacancies(request, vacancy_id):
+    vacancies = Vacancy.objects.all()  # from database
     return HttpResponse(f"<h1>Некая вакансия с неким номером {vacancy_id}<h1>")
 
 
 def relevance(request):
     path = os.path.abspath(os.path.curdir)
-    book = openpyxl.load_workbook(f'{path}\\main\\static\\main\\report.xlsx')
+    book = openpyxl.load_workbook(f'{path}/main/static/main/report.xlsx')
+    # book = openpyxl.load_workbook(f'{path}/DjangoSite/main/static/main/report.xlsx') # для сайта
     data = create_template(book)
     data['title'] = "Востребованность"
     return render(request, 'main/relevance.html', data)
 
 
 def create_template(book):
-    """Создает html шаблон pdf файла"""
     headings_years, data_years = get_formatted_data(book, 'Статистика по годам')
     headings_cities, data_cities = get_formatted_data(book, 'Статистика по городам', True)
     pdf_template = {'headings_years': headings_years, 'data_years': data_years,
-         'headings_cities': headings_cities, 'data_cities': data_cities}
+                    'headings_cities': headings_cities, 'data_cities': data_cities}
     return pdf_template
 
 
 def get_formatted_data(book, sheet_name, need_formatting=False):
-    """Возвращает данные для html шаблона в специальном формате
-
-    Args:
-        sheet_name (str): Название листа, из которого будут браться данные
-        need_formatting (bool): Показывает, нужно ли добавлять к ячейкам '%'
-
-    Returns:
-        (list): Список названий столбцов и данные
-    """
     data = []
     is_heading = True
     for row in book[sheet_name]:
@@ -76,44 +60,47 @@ def get_formatted_data(book, sheet_name, need_formatting=False):
 
 
 def last_vacancies(request):
-    url = 'https://api.hh.ru/dictionaries'
-    url = 'https://api.hh.ru/vacancies?text=gamedev&order_by=publication_time&only_with_salary=true'
+    dates = get_random_date()
+    url = f'https://api.hh.ru/vacancies?text=gamedev&date_from={dates["date_from"]}&date_to={dates["date_to"]}&order_by=publication_time&only_with_salary=true'
     response = requests.get(url, headers={"User-Agent": "api-test-agent"})
-    data = response.json()
-    vs = data['items']
-    print(vs)
-    vacancies = [get_correct_vacancy(v) for v in vs]
-    for v in vs:
-        print(v['published_at'])
-    # resume_search_order":[{"id":"publication_time","name":"по дате изменения"}
-    response = requests.get("https://api.hh.ru/vacancies/75582359", headers={"User-Agent": "api-test-agent"})
-    v = response.json()
+    given_vacancies = response.json()['items']
+    given_vacancies.reverse()
+    request_method = lambda x: requests.get(f"https://api.hh.ru/vacancies/{x}",
+                                            headers={"User-Agent": "api-test-agent"})
+    vacancies = []
+    for vacancy in given_vacancies[:5]:
+        current_vacancy = request_method(vacancy['id']).json()
+        current_vacancy = get_correct_vacancy(current_vacancy)
+        current_vacancy['published_at'] = parse_date(current_vacancy['published_at'])
+        vacancies.append(current_vacancy)
+    return render(request, "main/vacancies.html",
+                  {"vacancies": vacancies, 'title': 'Последнии вакансии'})
 
-    return render(request, "main/vacancies.html", {"vacancies": vacancies[:5], 'vacancy': v, 'title': 'Последнии вакансии'})
+
+def get_random_date():
+    day_from = random.randint(1, 31)
+    day_to = day_from + 5
+    if len(str(day_from)) == 1:
+        day_from = '0' + str(day_from)
+    if len(str(day_to)) == 1:
+        day_to = '0' + str(day_to)
+    elif int(day_to) > 26:
+        day_to = '31'
+    return {'date_from': f'2022-12-{day_from}', 'date_to': f'2022-12-{day_to}'}
+
+
+def parse_date(s):
+    if s is None or ('T' not in s and '-' not in s.split('T')):
+        return ""
+    time = s.split('T')[0].split('-')
+    time.reverse()
+    return '.'.join(time)
 
 
 def get_correct_vacancy(vacancy):
-    """Удаляет лишние символы из вакансии
-
-    Args:
-        vacancy (dict): Словарь, значения которого нужно очистить от лишних символов
-
-    Returns:
-        dict: Вакансия с корректными значениями
-    """
-
     def get_correct_string(s):
-        """Удаляет лишние пробелы и html теги из строки
-
-        Args:
-            s (str): Строка
-
-        Returns:
-            str: Очищенная строка
-        """
-        s = re.sub("[^a-zA-Z]",  # Search for all non-letters
-                              " ",  # Replace all non-letters with spaces
-                              str(s))
+        if not isinstance(s, str):
+            return s
         s = re.sub(r'<[^>]*>', '', s)
         result = []
         for item in s.split('\n'):
@@ -121,3 +108,12 @@ def get_correct_vacancy(vacancy):
         return '\n'.join(result)
 
     return {key: get_correct_string(vacancy[key]) for key in vacancy}
+
+
+def geography(request):
+    path = os.path.abspath(os.path.curdir)
+    book = openpyxl.load_workbook(f'{path}/main/static/main/report.xlsx')
+    # book = openpyxl.load_workbook(f'{path}/DjangoSite/main/static/main/report.xlsx') # для сайта
+    data = create_template(book)
+    data['title'] = "География"
+    return render(request, 'main/geography.html', data)
